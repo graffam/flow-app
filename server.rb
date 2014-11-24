c789e6178b93a4cab207dd92b410f1cb08dcfb48
graffam <graffam87@gmail.com>
2014-11-24 17:39:52 -0500

    added working flow dock chat posting, added some minor CSS

--- server.rb
+++ server.rb
@@ -3,6 +3,9 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'omniauth-github'
 require 'dotenv'
+require 'pg'
+require 'flowdock'
+require 'rubygems'
 
 Dotenv.load
 
@@ -17,16 +20,28 @@ end
 
 
 def user_from_omniauth(auth)
-  binding.pry
   {
     uid:auth.uid,
+    id: auth.id,
     provider: auth.provider,
     username:auth.info.nickname,
     avatar_url: auth.info.image
   }
 end
 
+def connect
+  begin
+    connection = PG.connect(dbname: 'flowquest')
+  end
+end
+
 def create_user(user_info)
+  sql = "INSERT INTO users (uid,username,avatar_url) VALUES(#{user_info[:uid]},#{user_info[:username]},#{user_info[:avatar_url]})"
+end
+
+def client
+  client = Flowdock::Client.new(api_token: )
+end
 
 get '/' do
   erb :index
@@ -35,5 +50,17 @@ end
 get '/auth/github/callback' do
   auth = env['omniauth.auth']
   user_info = user_from_omniauth(auth)
+  @flows = client.get('/flows')
+  erb :home
+end
 
+get '/flow' do
+  @api_token = params["token"]
+  erb :post
+end
+
+post '/flow_post' do
+  flow = Flowdock::Flow.new(:api_token => params["token"], source: "FlowQuest", external_user_name: "Eric")
+  flow.push_to_chat(content: params["content"], tags: ["zomg","kittens!"])
+  redirect '/flow'
 end
